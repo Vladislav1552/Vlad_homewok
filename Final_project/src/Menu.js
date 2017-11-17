9a25b47d465342246ca313f9c34a6dafe026c910
Vladislav1552 <vladiskav333@gmail.com>
2017-11-17 19:31:39 +0300

    Progect

---
+++ Final_project/src/Menu.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+import {Link} from 'react-router';
+import './app.css';
+//<Link to ='/Convector'>Convector </Link>
+ class Menu extends Component {
+    render() {
+      return (
+        <div>
+      Future menu
+   
+        </div>
+      );
+    }
+  } 
+  export default Menu;
---
+++ Final_project/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    context: path.resolve(__dirname, './src'),
+ 
+    entry: {
+        app: './app.js',
+        load: './load.js',
+        Menu:'./Menu.js',
+        Convector:'./Convector.js',
+        Table:'./Table.js',
+        Search:'./Search.js'
+    },
+    output: {
+        path: path.resolve(__dirname, './build/public'), //Изменили финальную директорию билда
+        filename: '[name].bundle.js',
+        publicPath: '/public' //Добавили публичную директорию (в нее и будет собираться весь наш код и другие ресурсы)
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, './src')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                             presets: ['es2015', 'react'] }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    
+    },
+
+    devtool: 'source-map'
+};
+    /*if (NODE_ENV == 'production') {
+        module.exports.plugins.push(
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false,
+                    drop_console: true
+                }
+            })
+        );
+    }
+    */
